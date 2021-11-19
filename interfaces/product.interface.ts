daae5ccdc3ff2a5b5b08ee640bdcb8a8b1655469
Mikhail <Virus1231@yandex.ru>
2021-11-19 11:31:57 +0300

    65
    65

---
+++ interfaces/product.interface.ts
@@ -0,0 +1,36 @@
+export interface ProductCharacteristic {
+  value: string;
+  name: string;
+}
+
+export interface ReviewModel {
+	_id: string;
+	name: string;
+	title: string;
+	description: string;
+	rating: number;
+	createdAt: Date;
+}
+
+export interface ProductModel {
+  _id: string;
+  categories: string[];
+  tags: string[];
+  title: string;
+  image: string;
+  description: string;
+  link: string;
+  price: number;
+  credit: number;
+  oldPrice: number;
+  characteristics: ProductCharacteristic[];
+  advantages: string;
+  initialRating: number;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: number;
+  html: string;
+  reviews: ReviewModel[];
+  reviewCount: number;
+  reviewAvg?: number;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,11 @@ import Paragraph from '../components/Paragraph/Paragraph';
 import Tag from '../components/Tag/Tag';
 import Rating from '../components/Rating/Rating';
 import { withLayout } from '../layout/Layout';
+import { GetStaticProps } from 'next';
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   const [counter, setCounter] = useState<number>(0);
 
   const [rating, setRating] = useState<number>(0);
@@ -34,8 +37,28 @@ function Home(): JSX.Element {
 
       <Rating rating={rating} isEditable={true} setRating={setRating}/>
 
+      <ul>
+        { menu.map( m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>) ) }
+      </ul>
+      
     </Fragment>
   );
 };
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {firstCategory});
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
