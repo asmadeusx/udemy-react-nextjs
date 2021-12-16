c4280047b7d104f6c864855f52997fd70edaf60a
Mikhail <Virus1231@yandex.ru>
2021-12-16 12:32:21 +0300

    92
    92

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -9,12 +9,20 @@ import Tag from '../Tag/Tag';
 import Button from '../Button/Button';
 import { declOfNum, priceRu } from '../../helpers/helpers';
 import Divider from '../Divider/Divider';
+import Image from 'next/image';
 
 
 function Product({ product, children, ...props }: ProductProps): JSX.Element {
   return (
 	  <Card className={styles.product}>
-		  <div className={styles.logo}><img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} /></div>
+		  <div className={styles.logo}>
+			  <Image 
+			  		src={process.env.NEXT_PUBLIC_DOMAIN + product.image} 
+					alt={product.title}
+					width={70}
+					height={70}
+			  />
+		  </div>
 		  <div className={styles.title}>{product.title}</div>
 		  <div className={styles.price}>
 			  {priceRu(product.price)}
