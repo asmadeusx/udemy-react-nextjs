4073f46ae07ba4f5c8e6eb3fe724be8fcd63977e
Mikhail <Virus1231@yandex.ru>
2022-01-20 12:19:15 +0300

    114
    114

--- components/Button/Button.props.ts
+++ components/Button/Button.props.ts
@@ -1,6 +1,9 @@
 import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from "react";
 
-export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+export interface ButtonProps extends 
+Omit<DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, 
+'onAnimationStart' | 'onDragStart' | 'onDragEnd' | 'onDrag' | 'ref'
+> {
   children: ReactNode;
   apperance: 'primary' | 'ghost';
   arrow?: 'right' | 'down' | 'none'; // Вопросительный знак значит, что параметр необязательный.
